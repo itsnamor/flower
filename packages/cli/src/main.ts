fbb144c5c03f1ec8920f5ded79e7d85d091cd5b3
luc.le <trungluc.dev@gmail.com>
2026-03-04 12:03:16 +0700

    release: v0.1.4

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -89,7 +89,7 @@ const init = defineCommand({
 const main = defineCommand({
   meta: {
     name: "flower",
-    version: "0.1.3",
+    version: "0.1.4",
     description: "🌸 flower CLI",
   },
   subCommands: {
